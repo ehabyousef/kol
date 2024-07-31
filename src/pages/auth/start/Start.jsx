60a6cba819bd99136866a6440a1a8a6afa480c2f
‪Ehab Yousef‬‏ <74505597+ehabyousef@users.noreply.github.com>
2024-08-01 01:38:26 +0300

    day

--- src/pages/auth/start/Start.jsx
+++ src/pages/auth/start/Start.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import style from './start.module.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaLongArrowAltRight } from "react-icons/fa";
 function Start() {
+    const navi = useNavigate()
     return (
         <div style={{ height: "100vh" }}>
             <div className="d-flex justify-content-center align-items-center flex-column gap-4 h-100 w-100">
@@ -13,7 +14,7 @@ function Start() {
                     <Link>User</Link>
                     <Link to='/auth/signblogger'>Blogger</Link>
                 </div>
-                <div className="d-flex gap-4 justify-content-center align-items-center">
+                <div className="d-flex gap-4 justify-content-center align-items-center" onClick={() => { navi('/auth/login') }} style={{ cursor: "pointer" }}>
                     <p className="m-0">already have an account</p>
                     <FaLongArrowAltRight className={style.icon} />
                 </div>
